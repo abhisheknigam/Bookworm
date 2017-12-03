94fabfe6e628589c75f34134c1c72ca9160e7ef6
karanvasnani <karanvasnani209@gmail.com>
2017-12-03 15:54:59 -0500

    added info dict

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -36,6 +36,7 @@ var currentState = states.START;
 
 var genreDict = { 'blues': 1, 'sad': 1, 'thriller': 2, 'horror': 2, 'crime': 2, 'children': 3, 'animal': 4, 'biography': 5, 'education': 6, 'Food and Fitness': 7, 'health': 8, 'Relationships': 9, 'Business': 10, 'Business Books': 10, 'Paperback Business Books': 10, 'Political': 12, 'Biography': 24, "Combined Print and E-Book Fiction": 13, "Combined Print and E-Book Nonfiction": 14, "Hardcover Fiction": 13, "Hardcover Nonfiction": 14, "Trade Fiction Paperback": 13, "Mass Market Paperback": 13, "Paperback Nonfiction": 14, "E-Book Fiction": 13, "E-Book Nonfiction": 14, "Hardcover Advice": 15, "Paperback Advice": 15, "Advice How-To and Miscellaneous": 15, "Chapter Books": 13, "Childrens Middle Grade": 3, "Childrens Middle Grade E-Book": 3, "Childrens Middle Grade Hardcover": 3, "Childrens Middle Grade Paperback": 3, "Paperback Books": 13, "Picture Books": 17, "Series Books": 13, "Young Adult": 16, "Young Adult E-Book": 16, "Young Adult Hardcover": 16, "Young Adult Paperback": 16, "Hardcover Graphic Books": 17, "Paperback Graphic Books": 17, "Manga": 17, "Combined Print Fiction": 13, "Combined Print Nonfiction": 14, "Animals": 18, "Business Books": 10, "Celebrities": 19, "Crime and Punishment": 2, "Culture": 6, "Education": 6, "Espionage": 20, "Expeditions Disasters and Adventures": 20, "Fashion Manners and Customs": 21, "Food and Fitness": 7, "Games and Activities": 22, "Hardcover Business Books": 10, "Health": 7, "Humor": 23, "Indigenous Americans": 24, "Relationships": 25, "Paperback Business Books": 10, "Family": 11, "Hardcover Political Books": 26, "Race and Civil Rights": 26, "Religion Spirituality and Faith": 30, "Science": 27, "Sports": 28, "Travel": 29 }
 var mapping = { 1: 'Advice How-To and Miscellaneous', 2: 'Crime and Punishment', 3: 'Childrens Middle Grade', 4: 'Animals', 5: 'Indigenous Americans', 6: 'Food and Fitness', 8: 'Health', 9: 'Relationships', 13: 'Combined Print and E-Book Fiction', 14: 'Combined Print and E-Book Nonfiction', 15: 'Advice How-To and Miscellaneous', 16: 'Young Adult', 17: 'Picture Books', 18: 'Animals', 19: 'Celebrities', 20: 'Expeditions Disasters and Adventures', 21: 'Fashion Manners and Customs', 22: 'Games and Activities', 23: 'Humor', 24: 'Indigenous Americans', 25: 'Relationships', 26: 'Hardcover Political Books', 27: 'Science', 28: 'Sports', 29: 'Travel', 30: 'Religion Spirituality and Faith', 11: 'Family' }
+var infoDict = { 'summary': false, 'rating': false, 'author': false }
 
 var BookInfo = function(nm, aut, rating, genre, summ) {
     this.name = nm;
@@ -143,6 +144,7 @@ exports.getBestSellerByDate = function(req, res) {
 }
 
 exports.recommendMeAbook = function(req, res) {
+    clearInfoDict();
     body = JSON.stringify(req.body);
     arr = [];
     var date = new Date().toJSON().slice(0, 10);
@@ -248,6 +250,24 @@ exports.getSummary = function(req, res) {
                     console.log(information);
                 });
             }
+
+            infoDict['summary'] = true;
+            var count = 0;
+
+            for (var key in infoDict){
+              if(infoDict[key] === false){
+                if(count == 0){
+                  msg += ". I can also tell you about its"
+                }
+                msg = msg + ", " + key;
+                count++;
+              }
+            }
+
+            if(count == 2){
+              msg = msg+ ". whenever you are satisfied you can add the book to your reading list."
+            }
+
             return res.status(200).json(information);
         });
     } else {
@@ -267,10 +287,29 @@ exports.getAuthor = function(req, res) {
         randomNumber = Math.floor(Math.random() * noBookSpecified.length);
         msg = noBookSpecified[randomNumber];
     }
+
+    infoDict['author'] = true;
+    var count = 0;
+
+    for (var key in infoDict){
+      if(infoDict[key] === false){
+        if(count == 0){
+          msg += ". I can also tell you about its"
+        }
+        msg = msg + ", " + key;
+        count++;
+      }
+    }
+
+    if(count == 2){
+      msg = msg+ ". whenever you are satisfied you can add the book to your reading list."
+    }
+
     return res.status(200).json(msg);
 }
 
 exports.getBookRecommendationByAuthor = function(req, res) {
+    clearInfoDict();
     var url_parts = urll.parse(req.url, true);
     var query = url_parts.query;
     console.log(JSON.stringify(query));
@@ -379,6 +418,7 @@ exports.getAllGenre = function(req, res) {
 }
 
 exports.getBookByGenre = function(req, res) {
+    clearInfoDict();
     var url_parts = urll.parse(req.url, true);
     var query = url_parts.query;
     console.log(JSON.stringify(query));
@@ -454,7 +494,14 @@ var searchBookByGenre = (req, res, genre) => {
     }
 }
 
+var clearInfoDict = () => {
+  for(var key in infoDict){
+    infoDict[key] = false;
+  }
+}
+
 exports.getAnotherBook = (req, res) => {
+    clearInfoDict();
     idx = books.indexOf(currentBook.name);
     delete books[idx];
     delete authors[idx];
@@ -497,6 +544,24 @@ exports.getBookRating = function(req, res) {
         randomNumber = Math.floor(Math.random() * noBookSpecified.length);
         var msg = noBookSpecified[randomNumber]
     }
+
+    infoDict['rating'] = true;
+    var count = 0;
+
+    for (var key in infoDict){
+      if(infoDict[key] === false){
+        if(count == 0){
+          msg += ". I can also tell you about its"
+        }
+        msg = msg + ", " + key;
+        count++;
+      }
+    }
+
+    if(count == 2){
+      msg = msg+ ". whenever you are satisfied you can add the book to your reading list."
+    }
+
     res.status(200).json(msg);
 }
 
